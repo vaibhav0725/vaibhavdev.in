fc0da09c0a18ae839375c4b0f356d40ffec9d759
vaibhav0725 <vaibhavwork0725@gmail.com>
2025-09-09 17:19:22 +0530

    react downgrade

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -11,7 +11,7 @@ export const Container = ({children,className}:{
             <div 
                 className="min-h-screen w-10 border-neutral-200 border-x-1 bg-white dark:bg-neutral-900 dark:border-neutral-800 bg-[image:var(--pattern-light)] dark:bg-[image:var(--pattern-dark)]"
             />
-            <div className={cn("min-h-screen w-5xl bg-white dark:bg-neutral-900",className)}>
+            <div className={cn("min-h-screen w-4xl bg-white dark:bg-neutral-900",className)}>
                 <NavBar/>
                 {children}
             </div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import { CiLinkedin } from "react-icons/ci";
 export const Footer = () => {
     return (
         <div className="w-[976px] bg-white dark:bg-neutral-900 mx-auto border-t-1 border-neutral-300 dark:border-neutral-700 text-neutral-500">
-            <div className="mx-auto w-5xl text-lg flex items-center justify-between px-5 py-2">
+            <div className="mx-auto w-4xl text-lg flex items-center justify-between px-5 py-2">
             <span className="text-xs">Built by Vaibhav</span>
             <div className="flex gap-5">
                 <CiLinkedin/>
